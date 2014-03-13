fbefae670a26150f437eb6090cf127c967cb208c
Dawid Kostyszak <d.kostyszak@gmail.com>
2014-03-13 16:13:00 +0100

    add models

--- project/collaboration/models.py
+++ project/collaboration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=30)

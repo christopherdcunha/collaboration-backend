c485410022422d63a81f9953c12ca2c4707fc970
Christopher D'Cunha <Chris.D'cunha@hogarthww.com>
2014-03-13 15:21:30 +0000

    move socket stuff around

--- project/collaboration/views.py
+++ project/collaboration/views.py
@@ -3,28 +3,14 @@ from socketio.namespace import BaseNamespace
 from socketio import socketio_manage
 from socketio.sdjango import namespace
 import logging
-
-
-@namespace('/chat')
-class ChatNamespace(BaseNamespace):
-    def initialize(self):
-        print 'socketio session started'
-        self.logger = logging.getLogger("socketio.chat")
-        self.log("Socketio session started")
-
-    def log(self, message):
-        self.logger.info("[{0}] {1}".format(self.socket.sessid, message))
-
-    def on_chat(self, msg):
-        print 'received: %s' % msg
-        self.emit('chat', 'you said: %s' % msg)
+from collaboration.sockets import ProjectNamespace
 
 
 def socketio(request):
     '''main notification view'''
     socketio_manage(
         request.environ,
-        {'/chat': ChatNamespace},
+        {'/project': ProjectNamespace},
         request
     )
 

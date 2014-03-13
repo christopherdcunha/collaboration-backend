f060dbc2c2fcf909dde9b1ad6570ca1a005f4fde
Christopher D'Cunha <Chris.D'cunha@hogarthww.com>
2014-03-13 16:07:39 +0000

    add post save signal

--- project/collaboration/models.py
+++ project/collaboration/models.py
@@ -1,3 +1,4 @@
+from django.db.models.signals import post_save
 from django.db import models
 
 
@@ -7,3 +8,11 @@ class Project(models.Model):
 
 class Job(models.Model):
     name = models.CharField(max_length=30)
+
+
+def publish(sender, **kwargs):
+    obj = kwargs['instance']
+    print 'the object is now saved.  %s' % obj
+
+post_save.connect(publish, sender=Project)
+post_save.connect(publish, sender=Job)
